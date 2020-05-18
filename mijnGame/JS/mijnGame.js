d1c48f5be8ca820b76f6ba141af2d7279985c15c
Rijk van Putten <rijkvanputten@hotmail.nl>
2020-05-18 12:04:18 +0000

    Add JS Polymorphism experiment

--- mijnGame/JS/mijnGame.js
+++ mijnGame/JS/mijnGame.js
@@ -1,84 +1,78 @@
-var data;
-var ringen = [];
+class Game {
+    constructor() {
+        this.gameObjects = this.createObjects();
+    }
 
-function preload() {
-    achtergrondmuziek = loadSound("sounds/bensound-dance.mp3");
-    // bron: https://www.bensound.com/royalty-free-music/track/dance
-    
-    raak = loadSound("sounds/score.wav");
-    data = loadJSON('assets/data.json');
+    createObjects() {
+        var objects = [];
+        for (var i = 0; i < 100; i++) {
+            const PADDING = 100;
+            objects.push(new Player(random(PADDING, windowWidth - PADDING), random(PADDING, windowHeight - PADDING), random(10, 100), random(10, 100), random(1, 5)));
+        }
+        return objects;
+    }
+
+    updateFrame() {
+        this.update();
+        this.draw();
+    }
+
+    update() {
+        this.gameObjects.forEach(gameObject => {
+            gameObject.update();
+        });
+    }
+
+    draw() {
+        background(100, 100, 100);
+        this.gameObjects.forEach(gameObject => {
+            gameObject.draw();
+        });
+    }
 }
 
+class GameObject {
+    constructor(x, y, width, height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    update() { }
+    draw() { }
+}
+
+class Player extends GameObject {
+    constructor(x, y, width, height, speed) {
+        super(x, y, width, height);
+        this.color = 'RED';
+        this.speed = speed;
+    }
+
+    update() {
+        this.x += this.speed;
+    }
+
+    draw() {
+        fill(this.color);
+        rect(this.x, this.y, this.width, this.height);
+    }
+}
+
+var game;
+
 function setup() {
-    dataNaarArray();
     createCanvas(windowWidth, windowHeight);
-    textFont("Monospace");
-    textSize(40);
-    textAlign(CENTER,CENTER);
-    b1 = new Bal();
-    d1 = new Doel();
+
+    game = new Game();
 }
 
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
-    d1.bepaalDiameter();
-    d1.x = canvas.width / 2;
-    d1.y = canvas.height / 2;
 }
 
 function draw() {
-    maakAchtergrond();
-    d1.teken();
-    b1.teken();
-    if (d1.raakt(b1)) {
-        if (!d1.wordtGeraakt) {
-            d1.geraakt++;
-            raak.play();
-            d1.wordtGeraakt = true;
-        }
-    }
-    else {
-        d1.wordtGeraakt = false;
-    }
-    if (b1.actief) {
-        b1.beweeg();
-    }
-    else {
-        background(255);
-        text('Pas de grootte van je browserscherm aan zodat de bal zo vaak mogelijk het doel in het midden raakt.\n\nDruk ENTER om te starten.',canvas.width / 4,canvas.height / 4,canvas.width / 2,canvas.height / 2);
-    }
-}
-
-function dataNaarArray() {
-    var ring,x,y,diameter;
-    var ringData = data['ringen'];
-    for (var i = 0; i < ringData.length; i++) {
-        ring = ringData[i];
-        x = ring['middelpunt']['x'];
-        y = ring['middelpunt']['y'];
-        diameter = ring['diameter'];
-        ringen.push(new Ring(x, y, diameter));
-  }
-}
-
-function maakAchtergrond() {
-    push();
-    background(255,0,200);
-    for (var i = 0; i < ringen.length; i++) {
-        ringen[i].teken();
-    }
-    text('Pas de grootte van je browserscherm aan zodat de bal zo vaak mogelijk het doel in het midden raakt.',0,0,canvas.width,canvas.height / 2);
-    textAlign(RIGHT,BOTTOM);
-    textSize(20);
-    text('Music: www.bensound.com',0,0,canvas.width,canvas.height);
-    pop();
-}
-
-function keyPressed() {
-  if (keyCode == ENTER) {
-    if (!b1.actief) {
-        b1.actief = true;
-        achtergrondmuziek.loop();
-    }
-  }
+    // GAME LOOP IS HANDELD BY THE GAME CLASS
+    game.updateFrame();
 }
