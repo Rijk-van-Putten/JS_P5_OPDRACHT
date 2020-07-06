87530159fd70c2b3d3e78b9ab9f599ad4d67457b
Rijk-van-Putten <rijkvanputten@hotmail.nl>
2020-07-06 18:56:39 +0200

    Add UI!

--- game/js/GameOverState.js
+++ game/js/GameOverState.js
@@ -2,31 +2,40 @@ class GameOverState extends State {
     constructor() {
         super();
         this.cameraPos = createVector(0, 0);
+        this.playAgainButton = new Button(0, 200, "PLAY AGAIN", this.playAgain);
+        this.menyButton = new Button(0, 300, "MENU", this.switchMenu);
+    }
+
+    playAgain() {
+        stateManager.switchState(GAME_STATE_ENUM.GAME);
+    }
+
+    switchMenu() {
+        stateManager.switchState(GAME_STATE_ENUM.MENU);
     }
 
     onUpdate() {
         camera(this.cameraPos.x, this.cameraPos.y, (height / 2.0) / tan(PI * 30.0 / 180.0), this.cameraPos.x, this.cameraPos.y, 0, 0, 1, 0);
-        if (keyIsPressed == true && focused) {
-            stateManager.switchState(GAME_STATE_ENUM.GAME);
-        }
+        this.playAgainButton.update();
+        this.menyButton.update();
     }
 
     onDraw() {
-        background(150, 150, 150);
+        background(200);
         textAlign(CENTER, CENTER);
         textSize(76);
-        fill('#000');
-        text("GAME OVER!", 0, 0);
-        fill('#fff');
+        fill('#a82f2f');
+        text("GIMMA OVER!", 0, -100);
+        fill('#222');
         textSize(32);
-        text("SCORE: " + gameOverInfo.score, 0, 90);
+        text("SCORE: " + gameOverInfo.score, 0, 20);
         var levelText;
         if (gameOverInfo.level <= 5)
             levelText = gameOverInfo.level;
         else 
             levelText = "ENDLESS";
-        text("LEVEL: " + levelText, 0, 140);
-        textSize(26);
-        text("PRESS ANY KEY TO PLAY AGAIN!", 0, 220);
+        text("LEVEL: " + levelText, 0, 100);
+        this.playAgainButton.draw();
+        this.menyButton.draw();
     }
 }
